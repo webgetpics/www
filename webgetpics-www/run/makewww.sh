8f13379e6e045d1c76cc2b3133e7bd9039b11508
Oleg Plakhotniuk <olegus8@gmail.com>
2015-04-28 20:46:53 -0500

    www

--- webgetpics-www/run/makewww.sh
+++ webgetpics-www/run/makewww.sh
@@ -18,5 +18,9 @@ cp -r /webgetpics-www/bootstrap.git/dist/* $TMPDIR
 cp -r /webgetpics-www/jquery/node_modules/jquery/dist/cdn/* $TMPDIR
 
 # pics
-convert $TMPDIR/rpi.jpg -strip -resize 800x600 $TMPDIR/rpi-sm.jpg
-convert $TMPDIR/screen.jpg -strip -resize 800x600 $TMPDIR/screen-sm.jpg
+convert $TMPDIR/rpi.jpg -strip \
+  -crop 2000x1500+1600+1300 \
+  -resize 800x600 $TMPDIR/rpi-sm.jpg
+convert $TMPDIR/screen.jpg -strip \
+  -crop 4000x3000+500+650 \
+  -resize 800x600 $TMPDIR/screen-sm.jpg
